03b12628a6ed6fd98d626fcd8af235e7ac04431e
Ganesh Bhusi <ganeshpaul999@gmail.com>
2020-06-25 11:44:11 +0530

    converted to functional components

--- src/components/Screens/CardsScreen.js
+++ src/components/Screens/CardsScreen.js
@@ -1,37 +1,51 @@
-import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import React from 'react';
+import {View} from 'react-native';
 import {ActionButton} from '../Usables/InputFields';
 import {clearDetails} from '../../actions';
-import {connect} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import CardsFetchComponent from '../Usables/CardsFetchComponent';
+import {USER_LOGOUT} from '../../constants';
 
-class CardsScreen extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  logoutUser = () => {
-    const {clearDetails} = this.props;
-    clearDetails('USER_LOGOUT');
+const CardsScreen = () => {
+  const dispatch = useDispatch();
+  const logoutUser = () => {
+    dispatch(clearDetails(USER_LOGOUT));
   };
+  return (
+    <View>
+      <CardsFetchComponent />
+      <ActionButton title={'Logout'} onPress={logoutUser} />
+    </View>
+  );
+};
+export default CardsScreen;
+// class CardsScreen extends Component {
+//   constructor(props) {
+//     super(props);
 
-  render() {
-    const {navigation} = this.props;
-    return (
-      <View>
-        <CardsFetchComponent navigation={navigation} />
-        <ActionButton title={'Logout'} onPress={this.logoutUser} />
-      </View>
-    );
-  }
-}
+//     this.state = {};
+//   }
 
-const mapStateToProps = state => ({
-  loggedInData: state.loginReducer.loggedInData,
-});
-export default connect(
-  mapStateToProps,
-  {clearDetails},
-)(CardsScreen);
+//   logoutUser = () => {
+//     const {clearDetails} = this.props;
+//     clearDetails('USER_LOGOUT');
+//   };
+
+//   render() {
+//     const {navigation} = this.props;
+//     return (
+//       <View>
+//         <CardsFetchComponent navigation={navigation} />
+//         <ActionButton title={'Logout'} onPress={this.logoutUser} />
+//       </View>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => ({
+//   loggedInData: state.loginReducer.loggedInData,
+// });
+// export default connect(
+//   mapStateToProps,
+//   {clearDetails},
+// )(CardsScreen);
--- src/components/Usables/CardsFetchComponent.js
+++ src/components/Usables/CardsFetchComponent.js
@@ -1,82 +1,129 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {View, Text, FlatList, ActivityIndicator} from 'react-native';
-import {
-  fetchCardsData,
-  fetchCardsDataError,
-  fetchCardsDataLoading,
-  fetchCardsDataSuccess,
-} from '../../actions';
-import {connect} from 'react-redux';
+import {fetchCardsData} from '../../actions';
+import {useDispatch, useSelector} from 'react-redux';
 import CardComponent from './CardComponent';
+import {useNavigation} from '@react-navigation/native';
 
-class CardsFetchComponent extends Component {
-  constructor(props) {
-    super(props);
+const CardsFetchComponent = () => {
+  const states = useSelector(state => state);
+  const {dataFetchReducer, loginReducer} = states;
+  const dispatch = useDispatch();
+  const {
+    cardsData,
+    cardsDataFailed,
+    cardsDataStarted,
+    cardsDataSuccess,
+  } = dataFetchReducer;
 
-    this.state = {};
-  }
-  componentDidMount() {
-    this.fetchAllCardsData();
-  }
+  const {loggedInData} = loginReducer;
 
-  fetchAllCardsData = () => {
-    const {loggedInData, fetchCardsData} = this.props;
+  useEffect(() => {
     let data = {
       userid: loggedInData.userid,
       action: 'fetchAllCards',
     };
-    fetchCardsData(data);
-  };
+    dispatch(fetchCardsData(data));
+  }, [dispatch, loggedInData.userid]);
+  const navigation = useNavigation();
 
-  cardDetailScreen = data => {
-    const {navigation} = this.props;
+  const cardDetailScreen = data => {
     navigation.navigate('CardDetailScreen', {
       cardId: data.id,
     });
   };
 
-  renderItem = ({item}) => {
+  const renderItem = ({item}) => {
     if (item.visible) {
-      return <CardComponent item={item} onPress={this.cardDetailScreen} />;
+      return <CardComponent item={item} onPress={cardDetailScreen} />;
     } else {
       return null;
     }
   };
-  render() {
-    const {cardsDataReducer} = this.props;
-    const {
-      cardsData,
-      cardsDataFailed,
-      cardsDataStarted,
-      cardsDataSuccess,
-    } = cardsDataReducer;
-    console.log('cardsDataReducer: ', cardsDataReducer);
-    return (
-      <View>
-        {cardsDataFailed && <Text>Failed to fetch data</Text>}
-        {cardsDataStarted && !cardsDataSuccess && !cardsDataFailed && (
-          <ActivityIndicator size={'large'} />
-        )}
-        <FlatList
-          data={cardsData}
-          renderItem={this.renderItem}
-          keyExtractor={item => item.id.toString()}
-        />
-      </View>
-    );
-  }
-}
 
-const mapStateToProps = state => ({
-  loggedInData: state.loginReducer.loggedInData,
-  cardsDataReducer: state.dataFetchReducer,
-});
-export default connect(
-  mapStateToProps,
-  {
-    fetchCardsData,
-    fetchCardsDataError,
-    fetchCardsDataLoading,
-    fetchCardsDataSuccess,
-  },
-)(CardsFetchComponent);
+  return (
+    <View>
+      {cardsDataFailed && <Text>Failed to fetch data</Text>}
+      {cardsDataStarted && !cardsDataSuccess && !cardsDataFailed && (
+        <ActivityIndicator size={'large'} />
+      )}
+      <FlatList
+        data={cardsData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
+    </View>
+  );
+};
+
+export default CardsFetchComponent;
+// class CardsFetchComponent extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {};
+//   }
+//   componentDidMount() {
+//     this.fetchAllCardsData();
+//   }
+
+// fetchAllCardsData = () => {
+//   const {loggedInData, fetchCardsData} = this.props;
+//   let data = {
+//     userid: loggedInData.userid,
+//     action: 'fetchAllCards',
+//   };
+//   fetchCardsData(data);
+// };
+
+// cardDetailScreen = data => {
+//   const {navigation} = this.props;
+//   navigation.navigate('CardDetailScreen', {
+//     cardId: data.id,
+//   });
+// };
+
+// renderItem = ({item}) => {
+//   if (item.visible) {
+//     return <CardComponent item={item} onPress={this.cardDetailScreen} />;
+//   } else {
+//     return null;
+//   }
+// };
+//   render() {
+// const {cardsDataReducer} = this.props;
+// const {
+//   cardsData,
+//   cardsDataFailed,
+//   cardsDataStarted,
+//   cardsDataSuccess,
+// } = cardsDataReducer;
+//     return (
+// <View>
+//   {cardsDataFailed && <Text>Failed to fetch data</Text>}
+//   {cardsDataStarted && !cardsDataSuccess && !cardsDataFailed && (
+//     <ActivityIndicator size={'large'} />
+//   )}
+//   <FlatList
+//     data={cardsData}
+//     renderItem={this.renderItem}
+//     keyExtractor={item => item.id.toString()}
+//   />
+// </View>
+//     );
+//   }
+// }
+
+// const mapStateToProps = state => ({
+//   loggedInData: state.loginReducer.loggedInData,
+//   cardsDataReducer: state.dataFetchReducer,
+// });
+// export default connect(
+//   mapStateToProps,
+//   {
+//     fetchCardsData,
+//     fetchCardsDataError,
+//     fetchCardsDataLoading,
+//     fetchCardsDataSuccess,
+//   },
+// )(CardsFetchComponent);
